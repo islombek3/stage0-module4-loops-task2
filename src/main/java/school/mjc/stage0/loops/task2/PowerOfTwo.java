def96702218fc6a3926acbf32ef34e41acec3ab2
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-25 12:08:23 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int counter = 0, i = 2;
+        if (power >= 0){
+            while (counter <= power){
+                int result = i ^ counter;
+                System.out.println(result); 
+            }
+        } else {
+            System.out.println("too much power");
+        }
     }
 }
