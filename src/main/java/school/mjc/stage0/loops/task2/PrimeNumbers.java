0abb8fdad7609d41b8e451bf702b182af66bde1b
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-25 12:09:00 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        while (counter <= printToInclusive){
+            if (ifPrime(counter) == true){
+                System.out.println(counter);
+            }
+            counter++;
+        }
+    }
+
+    public boolean ifPrime(int counter){
+        if (counter != 2 && counter % 2 == 0|| counter != 3 && counter % 3 == 0 || 
+        counter != 5 && counter % 5 == 0 || counter != 7 && counter % 7 == 0){
+            return false;
+        } else {
+            return true;
+        }
     }
 }
