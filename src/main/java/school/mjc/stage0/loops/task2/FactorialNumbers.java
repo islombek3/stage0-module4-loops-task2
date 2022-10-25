cb9af3c2c93625cda9428075a1807ed09fbc4ccf
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-25 12:06:25 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 1, result = 1;
+        System.out.println(result);
+        while (counter <= printToInclusive){
+            result *= counter;
+            System.out.println(result);
+            counter++;
+        }
 
     }
 }
